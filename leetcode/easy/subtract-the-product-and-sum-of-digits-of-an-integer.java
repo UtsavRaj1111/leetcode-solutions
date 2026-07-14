c33af52ba12e9958e8159400aebf50a2106c09a1
Utsav Raj Pratik <utsavrajpratik@gmail.com>
2026-07-15 01:10:28 +0530

    Add subtract-the-product-and-sum-of-digits-of-an-integer

---
+++ leetcode/easy/subtract-the-product-and-sum-of-digits-of-an-integer.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int subtractProductAndSum(int n) {
+        return helper(n,1,0);
+    }
+    private int helper(int n, int product, int sum){
+        if(n == 0){
+            return product - sum;
+        }
+        int digit = n % 10;
+    
+        return helper(n / 10, product * digit, sum + digit);
+    }  
+    }
