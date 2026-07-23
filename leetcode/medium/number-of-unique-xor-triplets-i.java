25b253dc55764f0af8d671702f82eb7cd50dbe35
Utsav Raj Pratik <utsavrajpratik@gmail.com>
2026-07-23 22:55:10 +0530

    Add number-of-unique-xor-triplets-i

---
+++ leetcode/medium/number-of-unique-xor-triplets-i.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+   
+        int n = nums.length; 
+
+        if(n <= 2) return n; 
+        
+
+        int bitWidth = 31; 
+        for(int i = 31; i>= 0; i--) {
+            if(((n >> i) & 1) == 1) {
+                break; 
+            }
+            bitWidth--; 
+        }
+
+        return (1 << (bitWidth + 1)); 
+    }
+}
